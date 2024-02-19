8523567a26c1fc7f935a96623960e5577d0d2c8b
unknown <wianw@naver.com>
2024-02-19 17:37:16 +0900

    2024-02-19 OWLBLOG

--- src/main.ts
+++ src/main.ts
@@ -29,8 +29,12 @@ async function bootstrap() {
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.enableCors({
     // origin: true,
-    origin: ["https://localhost:3000", "https://www.owlblog.site"],
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    origin: [
+      "https://localhost:3000",
+      "https://www.owlblog.site",
+      "https://owlblog.site",
+    ],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
     preflightContinue: false,
     exposedHeaders: ["refreshToken"],
     credentials: true,
